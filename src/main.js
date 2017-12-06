985e6fb2347a298ec116b58771653c6677b9c7d5
wuxiangqu <wuxiangqu@outlook.com>
2017-12-07 03:30:31 +0800

    commit all

--- src/main.js
+++ src/main.js
@@ -4,14 +4,48 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import store from './store'
-import ElementUI from 'element-ui'
+///import ElementUI from 'element-ui'
+import {
+  Form,
+  FormItem,
+  Input,
+  Menu,
+  Submenu,
+  MenuItem,
+  Table,
+  TableColumn,
+  Button,
+  Slider,
+  ColorPicker,
+  Message
+} from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
-import echarts from 'echarts'
+import echarts from 'echarts/lib/echarts'
+import 'echarts/lib/chart/bar'
+import 'echarts/lib/chart/line'
+import 'echarts/lib/chart/pie'
+import 'echarts/lib/component/tooltip'
+import 'echarts/lib/component/title'
+import 'echarts/lib/component/legend'
 import axios from 'axios'
 
-Vue.use(ElementUI)
+Vue.use(Form)
+Vue.use(FormItem)
+Vue.use(Input)
+Vue.use(Menu)
+Vue.use(Submenu)
+Vue.use(MenuItem)
+Vue.use(Table)
+Vue.use(TableColumn)
+Vue.use(Button)
+Vue.use(Slider)
+Vue.use(ColorPicker)
+//Vue.use(Message)
+Vue.component(Message.name, Message)
+
 Vue.prototype.$echarts = echarts
 Vue.prototype.$axios = axios
+Vue.prototype.$message = Message
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
@@ -20,5 +54,7 @@ new Vue({
   router,
   store,
   template: '<App/>',
-  components: { App }
+  components: {
+    App
+  }
 })
